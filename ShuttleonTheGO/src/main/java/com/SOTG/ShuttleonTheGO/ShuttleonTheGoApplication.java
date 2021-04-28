fb43d5ae09ff356b1a48148e8573217378238843
aniya <aniyanb00@gmail.com>
2021-04-28 18:06:17 -0400

    spring boot starter project

---
+++ ShuttleonTheGO/src/main/java/com/SOTG/ShuttleonTheGO/ShuttleonTheGoApplication.java
@@ -0,0 +1,13 @@
+package com.SOTG.ShuttleonTheGO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuttleonTheGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShuttleonTheGoApplication.class, args);
+	}
+
+}
---
+++ ShuttleonTheGO/src/test/java/com/SOTG/ShuttleonTheGO/ShuttleonTheGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SOTG.ShuttleonTheGO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShuttleonTheGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
